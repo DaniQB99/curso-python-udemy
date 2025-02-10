11f73ad0abf1b48b78ed90c904ea163b07fb96d2
Daniel Quirós Barroso <daniqb99@icloud.com>
2025-02-10 01:39:26 +0100

    Pandas + Jupyter

--- files/modules.py
+++ files/modules.py
@@ -5,7 +5,7 @@ import pandas
 while True:
     if os.path.exists('c:/Users/daniq/Desktop/PYTHON-UDEMY/curso-python-udemy/files/temps_today.csv'):
         data = pandas.read_csv('c:/Users/daniq/Desktop/PYTHON-UDEMY/curso-python-udemy/files/temps_today.csv')
-        print(data.mean())
+        print(data.mean()['st1'])
     else:
         print('File does not exist')
     time.sleep(10)
