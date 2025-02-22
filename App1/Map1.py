4fbb3cacc04797c640f74ede292bfc483d597b28
Daniel Quirós Barroso <daniqb99@icloud.com>
2025-02-22 09:36:19 +0100

    Maps1

--- App1/Map1.py
+++ App1/Map1.py
@@ -1,8 +1,17 @@
 import folium
-map = folium.Map(location=[38.950817, -5.867413], zoom_start=13, tiles='OpenStreetMap')
+import pandas
+
+data = pandas.read_csv('App1/Volcanoes.txt')
+lat = list(data['LAT'])
+lon = list(data['LON'])
+elev = list(data['ELEV'])
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=8, tiles='OpenStreetMap')
 
 fg = folium.FeatureGroup(name='My Map')
-fg.add_child(folium.Marker(location=[38.9508, -5.8674], popup="Hola soy el marker!", icon=folium.Icon(color='green')))
+
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+'m', icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 map.save('App1/Map1.html')
